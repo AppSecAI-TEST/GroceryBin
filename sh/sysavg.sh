bdeb94fac56c9d0d2ddc2c4d4547fb2fa0b9fb7e
yiji <canpodeyiji@gmail.com>
2017-08-11 14:15:24 +0800

    add a monitor bash script .

--- sh/sysavg.sh
+++ sh/sysavg.sh
@@ -1,11 +1,13 @@
 todo(){
-d=$(uptime)
-t=$(echo $d |grep -P -o '([0-9]{2}:){2}[0-9]{2}')
-s=$(echo $d |grep -P -o '[0-9]\.[0-9]{2}')
-echo $t $s>>/root/sysLoadAvg/sysavg
+d=$(date +'%Z %Y%m%d %H:%M:%S')
+upt=$(uptime)
+usr=$(echo $upt|grep -Poe '[0-9]+ user'|cut -d ' ' -f 1)
+avg=$(echo $upt|grep -Poe '[0-9]\.[0-9]{2}')
+echo $d ${usr}u $avg>>sysavg.log
 }
-for ((i=0;i<12;i++))
+
+for((i=0;i<${2};i++))
 do
 	todo
-	sleep 5
+	sleep $1
 done
